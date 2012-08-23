04b74d32b67e453cb3608a32c8e122b99897c981
Øyvind Ødegård <whirlwin90@gmail.com>
2012-08-24 00:21:44 +0200

    That was an ordeal!
    - With some tricks, the gem should work perfectly now :)

--- lib/bootstrappdf.rb
+++ lib/bootstrappdf.rb
@@ -36,11 +36,10 @@ begin
   unless ARGV[0].nil?
     kit = PDFKit.new send("convert_#{File.extname(ARGV[0])[1..-1]}", ARGV[0])
 
-    Dir.glob('res/bootstrap/css/*.css').each do |file|
-      kit.stylesheets << File.expand_path(file)
+    Dir.glob(File.join File.dirname(File.expand_path __FILE__),
+                       'res/bootstrap/css/*.css').each do |file|
+      kit.stylesheets << file
     end
-
-    #kit.stylesheets << File.expand_path('res/bootstrap/css/bootstrap.min.css')
 
     unless options[:css_dir].nil?
       Dir.glob("#{options[:css_dir]}/**/*.css").each do |file|
@@ -55,26 +54,3 @@ begin
 rescue Exception => e
   puts e, opt_parser
 end
-
-=begin
-task :haml do
-  files = Dir.glob '**/*.haml'
-
-  case files.length
-    when 0
-      raise 'Please provide at least one HAML file'
-    when 1..(1.0 / 0.0)
-      contents = files.each_with_object String.new do |input, output|
-        output << IO.read(input)
-      end
-
-      kit = PDFKit.new Haml::Engine.new(contents).render
-
-      Dir.glob('res/**/*.css').each do |file|
-        kit.stylesheets << file
-      end
-
-      kit.to_file 'out.pdf'
-  end
-end
-=end
